11ad4376083fa282e9df3b9e092a10f936ee0e16
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-12-20 14:02:43 -0300

    bump

--- viralizedfy/settings.py
+++ viralizedfy/settings.py
@@ -4,19 +4,15 @@ SPIDER_MODULES = ['viralizedfy.spiders']
 
 NEWSPIDER_MODULE = 'viralizedfy.spiders'
 
-LOG_LEVEL = 'DEBUG'
-
-ROBOTSTXT_OBEY = False
-
-RETRY_ENABLED = True
-
-RETRY_TIMES = 6
-
 MEDIA_ALLOW_REDIRECTS = True
 
 CONCURRENT_REQUESTS = 64
 
-FILES_STORE = 'foobar'
+FILES_STORE = 's3://viralizedfy/media'
+
+AWS_ACCESS_KEY_ID = 'AKIAS3BUSIV6LMWXD6VG'
+
+AWS_SECRET_ACCESS_KEY = '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'
 
 ITEM_PIPELINES = {
   'viralizedfy.pipelines.PreparePipeline': 100,
@@ -25,23 +21,31 @@ ITEM_PIPELINES = {
   # 'viralizedfy.pipelines.MarkdownifyPipeline': 1000
 }
 
-AWS_ACCESS_KEY_ID = 'AKIAS3BUSIV6LMWXD6VG'
-AWS_SECRET_ACCESS_KEY = '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'
-
+DELTAFETCH_ENABLED = True
 
 SPIDER_MIDDLEWARES = {
-    'scrapy_deltafetch.DeltaFetch': 300,
-    'scrapy_magicfields.MagicFieldsMiddleware': 600,
+    'scrapy_deltafetch.DeltaFetch': 100,
+    'scrapy_magicfields.MagicFieldsMiddleware': 200,
 }
 
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.httpcache.HttpCacheMiddleware': 100,
-    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 300,
-    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 600,
-    'scrapy.downloadermiddlewares.redirect.RedirectMiddleware': 900
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 200,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 300,
+    'scrapy.downloadermiddlewares.redirect.RedirectMiddleware': 400
 }
 
 MAGIC_FIELDS = {
   'url': "$response:url",
   'spider': '$spider:name',
 }
+
+EXTENSIONS = {
+  'scrapy_dotpersistence.DotScrapyPersistence': 0
+}
+
+DOTSCRAPY_ENABLED = True
+
+ADDONS_AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
+ADDONS_AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
+ADDONS_S3_BUCKET = 'viralizedfy'
